b926d9c27dfba6ceaafeda464a1fdec27f557568
Akshay Bhatia <emailakshaybhatia@gmail.com>
2018-11-10 19:19:00 -0800

    add GET users/me and auth middleware

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const _ = require('lodash')
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
+var {authenticate} = require('./middleware/authenticate')
 var {ObjectID} = require('mongodb')
 
 var app = express();
@@ -25,6 +26,7 @@ app.post('/todos', (req, res) => {
         res.status(400).send(e)
     })
 })
+
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
@@ -82,22 +84,19 @@ app.post('/users', (req,res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
-    user.save(body).then((result) => {
+    user.save(body).then(() => {
 
         return user.generateAuthToken();
-
-
-        if(!result) {
-            res.status(400).send();
-        }
-        console.log('Successfully saved user', result)
-        res.status(200).send()
     }).then((token) => {
         res.header('x-auth', token).send(user)
     }).catch((e) => {
          console.log('Error occurred', e);
          return res.status(400).send()
     })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user)
 })
 
 app.listen(port, () => {
